a120bc4d61ef3d22bc7759f6470b384d03888f0c
BeastHunter66 <andrelsandrade42@gmail.com>
2021-05-12 15:13:39 -0300

    Date capture function implementation

--- Chat_Node/app.js
+++ Chat_Node/app.js
@@ -13,7 +13,20 @@ function srv_list(req, res) {       //srv_list: config function of the httpServe
 httpServer.listen(port);
 
 //SOCKET CONFIGURATION / LISTENING.
-io.on("establish_conn", function(Socket){   //io: Socket identifier
+io.on("establish_conn", function(socket){   //io: Socket identifier
 
     /*Conection establishment and messages exchange. */
 });
+
+function getDate(){ // Date and Time capture and creation. Return the time in UTC.
+    var adate = new Date();
+    var day = (adate.getDate()< 10 ? '0' : '').adate.getDate();
+    var month = ((adate.getMonth() + 1) < 10 ? '0' : '') + (adate.getMonth() + 1);
+    var year = adate.getFullYear();
+    var hour = (adate.getHours() < 10 ? '0' : '') + adate.getHours();
+    var minutes = (adate.getMinutes() < 10 ? '0' : '') + adate.getMinutes();
+    var seconds = (adate.getSeconds() < 10 ? '0' : '') + adate.getSeconds();
+
+    var Formatdate = day + "/" + month + "/" + year + "/" + "-" + hour  + "/" + minutes + "/" + seconds + "/";
+    return Formatdate;
+}
