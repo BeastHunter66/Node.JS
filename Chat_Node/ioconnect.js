5f2028d9b66619f5ab4238f363b9b9fe859e1497
BeastHunter66 <andrelsandrade42@gmail.com>
2021-05-14 08:35:17 -0300

    v0.0.1C - Redesign of the whole client side logic.

--- Chat_Node/ioconnect.js
+++ Chat_Node/ioconnect.js
@@ -1,17 +1,9 @@
 //This Script performs the client side connection of the socket method.
-//This opens the connection and shend messages, as well as receives then. 
+//This opens the connection and send messages, as well as receives then. 
+//----------------------------------------------------------------------------------------------------------//
+
+// Modules Import:
+
 const io = require('socket.io');
-const host = 'localhost';
-const port  = 3000;
-var message = 'Hello World';
 
-//Socket connection.
-var socket = io.connect('http://127.0.0.1:3000');
-  socket.on('connect', function(){ //'connect': Event name for establish connection with the server.
-    socket.send(message);
-  });
-
-/*socket.on('message', function(message){
-    message = "Hello World !!!!";
-    socket.send(message);
-});*/
+//----------------------------------------------------------------------------------------------------------//
