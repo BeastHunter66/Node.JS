8cda700e2c096ae29f3de506a505b95cbae3fcc1
BeastHunter66 <andrelsandrade42@gmail.com>
2021-05-14 14:55:28 -0300

    V0.0.1D preparations.

--- Chat_Node/ioconnect.js
+++ Chat_Node/ioconnect.js
@@ -1,5 +1,6 @@
 //This Script performs the client side connection of the socket method.
 //This opens the connection and send messages, as well as receives then. 
+//This uses a hybrid version of PHP + JS to get the data from the JQuery and send it to the 
 //----------------------------------------------------------------------------------------------------------//
 
 // Modules Import:
